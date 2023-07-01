5e28e9bfd4245ec3da639a5a8066426c29db0944
Sabeerwaqas <sabeerwaqas050@gmail.com>
2023-07-01 12:47:22 +0500

    Make a component

--- src/components/five_star/FiveStar.js
+++ src/components/five_star/FiveStar.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import "./style.css"
 
 const FiveStar = () => {
   return (
-    <div>FiveStar</div>
+   <>
+    <div className='main-div'>
+    <div className='image-link'>
+      <img className='random-image' src="https://picsum.photos/seed/picsum/205/130" alt="Random Image" />
+    </div>
+    <div className='stars'>
+
+    </div>
+
+    </div>
+   </>
   )
 }
 
